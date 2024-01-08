86499705c718214955f3a87f8d06b9ac1e1bb3a0
ashith <mail2anandashith@gmail.com>
2024-01-08 14:01:44 +1100

    End of Section 4 - React Router

--- Client/src/features/catalog/ProductCard.tsx
+++ Client/src/features/catalog/ProductCard.tsx
@@ -11,6 +11,7 @@ import {
     CardHeader,
   } from "@mui/material";
   import { Product } from "../../app/models/products";
+import { Link } from "react-router-dom";
   
   interface Props {
     product: Product;
@@ -50,7 +51,7 @@ import {
           </CardContent>
           <CardActions>
             <Button size="small">Add to cart</Button>
-            <Button size="small">View</Button>
+            <Button size="small" component={Link} to= {`/catalog/${product.id}`}>View</Button>
           </CardActions>
         </Card>
       </>
